6cade2d95d5a6339dbb565293dd3fffcceaba97d
Sudhaharniie Thevar <thevarsudha11@gmail.com>
2025-04-09 16:58:07 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react"
-import Card from '../src/components/Card'
 import logo from "./assets/logo.png"
-
+import Card from './components/Card'
 
 function App() {
 
